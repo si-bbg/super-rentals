ee598dab99b6f8d3cb3e2cd3ea6b24b8ef10a106
si-bbg <babak_91@hotmail.com>
2017-04-08 13:48:12 -0400

    hbs helper

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isWide: false
+  isWide: false,
   actions: {
     toggleImageSize() {
       this.toggleProperty('isWide');
--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -18,9 +18,16 @@ moduleForComponent('rental-listing', 'Integration | Component | rental listing',
 test('should display rental details', function(assert) {
   this.set('rentalObj', rental);
   this.render(hbs`{{rental-listing rental=rentalObj}}`);
+  assert.equal(this.$('.listing h3').text(), 'test-title', 'Title: test-title');
+  assert.equal(this.$('.listing .owner').text().trim(), 'Owner: test-owner');
 });
 
 test('should toggle wide class on click', function(assert) {
   this.set('rentalObj', rental);
   this.render(hbs`{{rental-listing rental=rentalObj}}`);
+  assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
+  this.$('.image').click();
+  assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
 });
